88e910594e122349f4e473d488f823673f17b231
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-07-12 17:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
-}
+	if(s ===''){
+		return s;
+	}
+	const firstSpaceIndex = s.indexOf(' ');
+	if(firstSpaceIndex === -1){
+		return s;
+	}
+	 return s.slice(0, firstSpaceIndex);
+	}
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
